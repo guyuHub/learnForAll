e97c6644a95987535f11a28b0460a179a8206ce8
guyu <guyu@bixin.cn>
2022-08-23 19:53:05 +0800

    初次提交

---
+++ src/main/java/com/example/learnForAll/LearnForAllApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnForAll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnForAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnForAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learnForAll/LearnForAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnForAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnForAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
